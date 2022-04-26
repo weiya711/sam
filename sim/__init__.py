1fd1088533492f4ff926d1f6a079ec08f7c256e2
mbstrange2 <mstrange@stanford.edu>
2022-04-26 11:11:33 -0700

    get rid of old files

--- sim/__init__.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-# we want to have pytest assert introspection in the helpers
-pytest.register_assert_rewrite('sim.src.primitives')
-pytest.register_assert_rewrite('sim.test')
--- sim/test/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-def pytest_addoption(parser):
-    parser.addoption("--debug-sim", action="store_true", default=False, help="Emit debug print statements. Use with `-s`")
-
-@pytest.fixture
-def debug_sim(request):
-    return request.config.getoption("--debug-sim")
