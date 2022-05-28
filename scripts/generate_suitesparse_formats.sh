ba51c42a2f15de5ce57925af6e7e7dcff525fe17
Olivia Hsu <owhsu@stanford.edu>
2022-05-28 15:38:17 -0700

    Add in files to cast to int while preserving sparsity

--- scripts/generate_suitesparse_formats.sh
+++ scripts/generate_suitesparse_formats.sh
@@ -29,5 +29,6 @@ for i in ${!DATASET_NAMES[@]}; do
     name=${DATASET_NAMES[$i]} 
     echo "Generating input format files for $name..."
     python $basedir/scripts/datastructure_suitesparse.py -n $name 
-    chmod -R 777 $outdir
+    chgrp -R sparsity $outdir
+    chmod -R 775 $outdir
 done
