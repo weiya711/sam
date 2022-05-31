74b466fd229001b96e1749f234c58bf1fccd727b
Ritvik Sharma <rsharma3@kiwi.stanford.edu>
2022-05-31 14:37:10 -0700

    simulator fix?

--- sam/sim/test/apps/test_vecmul.py
+++ sam/sim/test/apps/test_vecmul.py
@@ -49,16 +49,16 @@ def test_vecmul_i(ssname, debug_sim, fill=0):
 
     fiberlookup_Bi_12 = UncompressCrdRdScan(dim=B_shape[0], debug=debug_sim)
     fiberlookup_Bj_7 = CompressedCrdRdScan(crd_arr=B_crd1, seg_arr=B_seg1, debug=debug_sim)
-    fiberwrite_x0_5 = CompressWrScan(seg_size=2, size=B_shape[0], fill=fill, debug=debug_sim)
+    fiberwrite_x0_0 = CompressWrScan(seg_size=2, size=B_shape[0], fill=fill, debug=debug_sim)
     repsiggen_i_10 = RepeatSigGen(debug=debug_sim)
     repeat_ci_9 = Repeat(debug=debug_sim)
     fiberlookup_cj_8 = CompressedCrdRdScan(crd_arr=c_crd0, seg_arr=c_seg0, debug=debug_sim)
     intersectj_6 = Intersect2(debug=debug_sim)
-    arrayvals_B_3 = Array(init_arr=B_vals, debug=debug_sim)
-    arrayvals_c_4 = Array(init_arr=c_vals, debug=debug_sim)
-    mul_2 = Multiply2(debug=debug_sim)
-    reduce_1 = Reduce(debug=debug_sim)
-    fiberwrite_xvals_0 = ValsWrScan(size=1 * B_shape[0], fill=fill, debug=debug_sim)
+    arrayvals_B_4 = Array(init_arr=B_vals, debug=debug_sim)
+    arrayvals_c_5 = Array(init_arr=c_vals, debug=debug_sim)
+    mul_3 = Multiply2(debug=debug_sim)
+    reduce_2 = Reduce(debug=debug_sim)
+    fiberwrite_xvals_1 = ValsWrScan(size=B_shape[0], fill=fill, debug=debug_sim)
     in_ref_B = [0, 'D']
     in_ref_c = [0, 'D']
     done = False
@@ -72,8 +72,8 @@ def test_vecmul_i(ssname, debug_sim, fill=0):
         fiberlookup_Bj_7.set_in_ref(fiberlookup_Bi_12.out_ref())
         fiberlookup_Bj_7.update()
 
-        fiberwrite_x0_5.set_input(fiberlookup_Bi_12.out_crd())
-        fiberwrite_x0_5.update()
+        fiberwrite_x0_0.set_input(fiberlookup_Bi_12.out_crd())
+        fiberwrite_x0_0.update()
 
         repsiggen_i_10.set_istream(fiberlookup_Bi_12.out_crd())
         repsiggen_i_10.update()
@@ -90,40 +90,40 @@ def test_vecmul_i(ssname, debug_sim, fill=0):
         intersectj_6.set_in2(fiberlookup_Bj_7.out_ref(), fiberlookup_Bj_7.out_crd())
         intersectj_6.update()
 
-        arrayvals_B_3.set_load(intersectj_6.out_ref2())
-        arrayvals_B_3.update()
+        arrayvals_B_4.set_load(intersectj_6.out_ref2())
+        arrayvals_B_4.update()
 
-        arrayvals_c_4.set_load(intersectj_6.out_ref1())
-        arrayvals_c_4.update()
+        arrayvals_c_5.set_load(intersectj_6.out_ref1())
+        arrayvals_c_5.update()
 
-        mul_2.set_in1(arrayvals_B_3.out_load())
-        mul_2.set_in2(arrayvals_c_4.out_load())
-        mul_2.update()
+        mul_3.set_in1(arrayvals_B_4.out_load())
+        mul_3.set_in2(arrayvals_c_5.out_load())
+        mul_3.update()
 
-        reduce_1.set_in_val(mul_2.out_val())
-        reduce_1.update()
+        reduce_2.set_in_val(mul_3.out_val())
+        reduce_2.update()
 
-        fiberwrite_xvals_0.set_input(reduce_1.out_val())
-        fiberwrite_xvals_0.update()
+        fiberwrite_xvals_1.set_input(reduce_2.out_val())
+        fiberwrite_xvals_1.update()
 
-        done = fiberwrite_x0_5.out_done() and fiberwrite_xvals_0.out_done()
+        done = fiberwrite_x0_0.out_done() and fiberwrite_xvals_1.out_done()
         time += 1
 
-    fiberwrite_x0_5.autosize()
-    fiberwrite_xvals_0.autosize()
+    fiberwrite_x0_0.autosize()
+    fiberwrite_xvals_1.autosize()
 
-    out_crds = [fiberwrite_x0_5.get_arr()]
-    out_segs = [fiberwrite_x0_5.get_seg_arr()]
-    out_vals = fiberwrite_xvals_0.get_arr()
+    out_crds = [fiberwrite_x0_0.get_arr()]
+    out_segs = [fiberwrite_x0_0.get_seg_arr()]
+    out_vals = fiberwrite_xvals_1.get_arr()
     f = open("../" + ssname + ".csv", "a")
     writer = csv.writer(f)
     repsiggen_i_10.print_fifos()
     repeat_ci_9.print_fifos()
     intersectj_6.print_fifos()
-    arrayvals_B_3.print_fifos()
-    mul_2.print_fifos()
-    reduce_1.print_fifos()
-    arrayvals_c_4.print_fifos()
+    arrayvals_B_4.print_fifos()
+    mul_3.print_fifos()
+    reduce_2.print_fifos()
+    arrayvals_c_5.print_fifos()
     intersectj_6.print_intersection_rate()
     writer.writerow(["vecmul","intersectj_6", str(intersectj_6.return_intersection_rate())])
     f.close()
--- sam/sim/test/test_gold.py
+++
@@ -1,8 +0,0 @@
-def test_gold_matmul(ssname , formats,  out_crds, out_segs, out_vals):
-        return 1
-def test_gold_vec(ssname , formats,  out_crds, out_segs, out_vals):
-        return 1
-def test_gold_vecmul(ssname , formats,  out_crds, out_segs, out_vals):
-        return 1
-def test_gold_vecmul(ssname , formats,  out_crds, out_segs, out_vals):
-        return 1
