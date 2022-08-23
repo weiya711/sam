3b8f2ca13d01acda9ea9fb10401f8bf02bde584a
Olivia Hsu <owhsu@stanford.edu>
2022-08-23 10:07:20 -0700

    Add in reduce to pe conn

--- sam/onyx/hw_nodes/reduce_node.py
+++ sam/onyx/hw_nodes/reduce_node.py
@@ -50,7 +50,7 @@ class ReduceNode(HWNode):
         elif other_type == ReduceNode:
             other_red = other.get_name()
             new_conns = {
-                'reduce_to_wr_scan': [
+                'reduce_to_reduce': [
                     # send output to rd scanner
                     ([(red, "data_out"), (other_red, "data_in")], 17),
                     # ([(red, "eos_out"), (wr_scan, "eos_in_0")], 1),
@@ -66,7 +66,18 @@ class ReduceNode(HWNode):
         elif other_type == RepeatNode:
             raise NotImplementedError(f'Cannot connect ReduceNode to {other_type}')
         elif other_type == ComputeNode:
-            raise NotImplementedError(f'Cannot connect ReduceNode to {other_type}')
+            pe = other.get_name()
+            other_conn = other.get_num_inputs()
+            new_conns = {
+                'reduce_to_pe_{other_conn}': [
+                    # send output to rd scanner
+                    ([(red, "data_out"), (pe, "data{other_conn}")], 17),
+                    # ([(red, "eos_out"), (wr_scan, "eos_in_0")], 1),
+                    # ([(wr_scan, "ready_out_0"), (red, "ready_in")], 1),
+                    # ([(red, "valid_out"), (wr_scan, "valid_in_0")], 1),
+                ]
+            }
+            return new_conns
         elif other_type == BroadcastNode:
             raise NotImplementedError(f'Cannot connect ReduceNode to {other_type}')
         elif other_type == RepSigGenNode:
