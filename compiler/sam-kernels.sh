c865d09fbb98a1aa3bd1bb13d582363f665fb641
Olivia Hsu <owhsu@stanford.edu>
2022-08-26 14:44:17 -0700

    Add in hand-modified compression tests

--- compiler/sam-kernels.sh
+++ compiler/sam-kernels.sh
@@ -37,13 +37,17 @@ GEN_KERNEL_NAMES=(
   mat_residual_WRONG
   mat_elemadd3
   tensor3_mttkrp
-  spacc_vec_simple
-  spacc_mat_simple
+  vec_spacc_simple
+  mat_spacc_simple
+  vec_sd_compression_WRONG
+  vec_ds_compression_WRONG
 )
 
 HAND_KERNEL_NAMES=(
   mat_residual
   mat_mattransmul
+  vec_sd_compression
+  vec_ds_compression
 )
 
 TACO_ARGS=(
@@ -77,6 +81,8 @@ TACO_ARGS=(
   "X(i,j)=B(i,k,l)*C(j,k)*D(j,l) -f=X:ss -f=B:sss -f=C:ss -f=D:ss" 
   "x(j)=B(i,j) -f=x:s -f=B:ss"
   "X(j,k)=B(i,j,k) -f=X:ss -f=B:sss"
+  "x(i)=b(i) -f=b:s -f=x:d"
+  "x(i)=b(i) -f=b:d -f=x:s"
 )
 
 mkdir -p $dir
