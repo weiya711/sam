da6fafe1623d13b8ba43d3f995f76eee8ccbf77f
Maxwell Strange <mstrange@stanford.edu>
2023-01-18 10:50:10 -0800

    updates for running current

--- sam/onyx/suitesparse.py
+++ sam/onyx/suitesparse.py
@@ -88,17 +88,43 @@ def run_build_tb(log_file, basedir, sparse_test_basedir, benchname, matrix_tmp_d
                                                     f"sam/compiler/sam-outputs/dot/{benchname}.gv"), "--seed", f"{0}",
                         "--dump_bitstream", "--add_pond", "--combined",
                         "--pipeline_scanner", "--base_dir", sparse_test_basedir,
-                        "--fiber_access", "--give_tensor", "--matrix_tmp_dir", matrix_tmp_dir, "--trace"]
-    error = run_process(build_tb_command, log_file, check=check)
+                        "--fiber_access", "--give_tensor", "--dump_glb", "--matrix_tmp_dir", matrix_tmp_dir]
+                        #"--fiber_access", "--give_tensor", "--gen_verilog", "--gen_pe", "--dump_glb", "--matrix_tmp_dir", matrix_tmp_dir]
+                        #"--fiber_access", "--give_tensor", "--trace"]
+                        #"--fiber_access", "--give_tensor", "--matrix_tmp_dir", matrix_tmp_dir, "--trace"]
+    error, output_txt = run_process(build_tb_command, log_file, check=check, return_stdout=True)
 
-    return error
+    print("OUTPUT TXT")
+    #print(output_txt)
+
+    split_ot = output_txt.split("\n")
+
+    cyc_count = 0
+
+    for line in split_ot:
+        if "Cycle" in line:
+            print(line)
+            ls = line.split()
+            cyc_count = int(ls[-1]) 
+        #print(line)
+
+    return error, cyc_count
 
 
-def run_process(command, log_file=None, cwd=None, split=False, check=True):
+def run_process(command, log_file=None, cwd=None, split=False, check=True, return_stdout=False):
     if split:
         command = command.split()
 
-    return_code = subprocess.run(command, cwd=cwd, check=check).returncode
+    run_result = subprocess.run(command, cwd=cwd, check=check, capture_output=True, text=True)
+    #return_code = subprocess.run(command, cwd=cwd, check=check).returncode
+    return_code = run_result.returncode
+    output_txt = run_result.stdout
+
+    cycle_count = [line_ for line_ in output_txt if "Cycle Count" in line_]
+
+    print("FOUND CYCLE COUNT")
+    print(cycle_count)
+    #exit()
 
     error = return_code != 0
     if error:
@@ -106,10 +132,13 @@ def run_process(command, log_file=None, cwd=None, split=False, check=True):
         log_file = "./suitesparse_run.log" if log_file is None else log_file
         log_error(log_file, return_code, " ".join(command))
 
-    return error
+    if return_stdout:
+        return error, output_txt
+    else:
+        return error
 
 
-def run_bench(benchname, args, matrices):
+def run_bench(benchname, args, matrices, stats):
     cwd = os.getcwd()
 
     basedir = "/aha" if args.docker else cwd
@@ -139,11 +168,15 @@ def run_bench(benchname, args, matrices):
             continue
         if generate_suitesparse(log_file, basedir, benchname, mtx, mtx_dir, matrix_tmp_dir, check):
             continue
-        if run_build_tb(log_file, basedir, sparse_test_basedir, benchname, matrix_tmp_dir, check):
+        err, cyc_count = run_build_tb(log_file, basedir, sparse_test_basedir, benchname, matrix_tmp_dir, check)
+        #if run_build_tb(log_file, basedir, sparse_test_basedir, benchname, matrix_tmp_dir, check):
+        if err:
             continue
         else:
             with open(log_file, "a+") as lf:
                 lf.write("SUCCESS\n")
+            pstr = f"{benchname}, {mtx}, {cyc_count}\n"
+            stats.append(pstr)
 
         clean_dir(mtx_dir)
 
@@ -158,25 +191,34 @@ if __name__ == "__main__":
     parser.add_argument('--log', type=str, default=None)
     parser.add_argument('--append_log', action="store_true")
     parser.add_argument('--continue_run', action="store_true")
+    parser.add_argument('--perf_log', type=str, default=None)
     args = parser.parse_args()
 
     benchmarks = ["matmul_ijk", "mat_elemmul", "mat_elemadd", "mat_elemadd3"]
+
+    full_stats = ["BENCH, MATRIX, CYCLE_COUNT\n"]
 
     # Get matrices
     matrices = None
     with open(args.matrix_file, "r") as ff:
         matrices = ff.read().splitlines()
+        print(matrices)
     assert matrices is not None, "Error opening file " + args.matrix_file
 
     # Run on all benchmarks
     if args.benchname == "all":
         for benchname in benchmarks:
             print("Running for bench", benchname, "...")
-            run_bench(benchname, args, matrices)
+            run_bench(benchname, args, matrices, full_stats)
     # Only run on one
     else:
         benchname = args.benchname
         assert benchname in benchmarks
 
         print("Running for bench", benchname, "...")
-        run_bench(benchname, args, matrices)
+        run_bench(benchname, args, matrices, full_stats)
+
+    if args.perf_log is not None:
+        with open(args.perf_log, "w+") as plog_file:
+            plog_file.writelines(full_stats)
+
