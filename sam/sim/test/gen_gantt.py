06b9b0c149a018aeb5d27d8acd77feb705d56ae3
Gina Sohn <gina7484@gmail.com>
2023-02-10 10:03:35 -0800

    gantt chart bars in a consistent order

--- sam/sim/test/gen_gantt.py
+++ sam/sim/test/gen_gantt.py
@@ -16,15 +16,22 @@ def gen_gantt(extra_info, testname):
             finish_c = extra_info[k]
         elif ("start_cycle" in k) and (sam_name in k.split('/')[0]):
             start_c = extra_info[k]
-            block_list.append(sam_name)
+            # We assume that the info to extra_info is added in the same order
+            # each block is updated
+            block_list.insert(0, sam_name)
+            # block_list.append(sam_name)
             if not isinstance(start_c, int):
-                start_list.append(int(start_c))
-                duration_list.append(finish_c - int(start_c))
+                # start_list.append(int(start_c))
+                # duration_list.append(finish_c - int(start_c))
+                start_list.insert(0, int(start_c))
+                duration_list.insert(0, finish_c - int(start_c))
             else:
-                start_list.append(start_c)
-                duration_list.append(finish_c - start_c)
+                # start_list.append(start_c)
+                # duration_list.append(finish_c - start_c)
+                start_list.insert(0, start_c)
+                duration_list.insert(0, finish_c - start_c)
 
     plt.barh(y=block_list, width=duration_list, left=start_list)
-
+    
     file_name = testname + '_' + extra_info["dataset"] + ".png"
     plt.savefig(file_name, bbox_inches="tight")
