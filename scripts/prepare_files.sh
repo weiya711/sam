9c3109bd1b16cb27d0c2cca473bb721f8851385e
Olivia Hsu <owhsu@stanford.edu>
2023-02-17 10:18:48 -0800

    Add in scripts for onyx tiling

--- scripts/prepare_files.sh
+++ scripts/prepare_files.sh
@@ -9,6 +9,6 @@ basedir=$(pwd)
 
 rm -rf $basedir/tiles/*
 
-./scripts/tile_ext.sh $1 memory_config_extensor_17M_llb.yaml
+./scripts/tile_ext.sh $1 $2
 
-python scripts/generate_gold_matmul_tiled.py --yaml_name memory_config_extensor_17M_llb.yaml
+python scripts/generate_gold_matmul_tiled.py --yaml_name $2
