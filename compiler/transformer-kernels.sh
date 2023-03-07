cfba456f4e0246f71cba6cf95adeb26595aa97bc
Rubens Lacouture <48832245+lrubens@users.noreply.github.com>
2023-03-07 11:46:01 -0800

    Update transformer-kernels.sh

--- compiler/transformer-kernels.sh
+++ compiler/transformer-kernels.sh
@@ -9,15 +9,15 @@ png_dir=$dir/png
 taco_exe=./taco/build/bin/taco
 
 KERNEL_NAMES=(
-  tensor3_linear
+#   tensor3_linear
   tensor4_multiply2
 )
 
 
 TACO_ARGS=(
-  "X(i,j,k)=B(j,l)*C(i,l,k)+D(j) -f=X:sss:0,2,1 -f=B:ss:1,0 -f=C:sss:0,2,1 -s=reorder(i,k,l,j)" #reorder: jikl 
-  "X(i,j,k)=B(j,l)*C(i,l,k) -f=X:sss:0,2,1 -f=B:ss:1,0 -f=C:sss:0,2,1" #reorder: jikl 
-  "X(i,j,k)=C(i,l,k)+D(j) -f=X:sss:0,2,1 -f=B:ss:1,0 -f=C:sss:0,2,1 -s=reorder(i,k,l,j)" #reorder: jikl 
+#   "X(i,j,k)=B(j,l)*C(i,l,k)+D(j) -f=X:sss:0,2,1 -f=B:ss:1,0 -f=C:sss:0,2,1 -s=reorder(i,k,l,j)" #reorder: jikl 
+#   "X(i,j,k)=B(j,l)*C(i,l,k) -f=X:sss:0,2,1 -f=B:ss:1,0 -f=C:sss:0,2,1" #reorder: jikl 
+#   "X(i,j,k)=C(i,l,k)+D(j) -f=X:sss:0,2,1 -f=B:ss:1,0 -f=C:sss:0,2,1 -s=reorder(i,k,l,j)" #reorder: jikl 
   # "X(i,k,j,m)=B(i,j,k,l)*C(i,l,j,m) -f=X:ssss:0,1,2,3 -f=B:ssss:0,2,1,3 -f=C:ssss:0,3,1,2 -s=reorder(i,k,j,m,l)"
   # "X(i,k,j,m)=B(i,j,k,l)*C(i,l,j,m) -f=X:ssss:0,1,2,3 -f=B:ssss:0,2,1,3 -f=C:ssss:0,3,1,2 -s=reorder(i,k,j,m,l)"
   "X(i,k,j,m)=B(i,j,k,l)*C(i,l,j,m) -f=X:ssss:0,1,2,3 -f=B:ssss:0,2,1,3 -f=C:ssss:0,2,3,1 -s=reorder(i,k,j,m,l)"
