85401559698102ff79cda1faa364af2edfc9cc55
Bo Wun Cheng <bwcheng@stanford.edu>
2023-10-16 23:47:24 -0700

    include sam simulation code that models compression using crddrop

--- sam/sim/src/compression.py
+++ sam/sim/src/compression.py
@@ -1,14 +1,26 @@
 from .base import *
+from .token import EmptyFiberStknDrop, StknDrop
 
 
-class Compression(Primitive):
-    def __init__(self, **kwargs):
+class ValDropper(Primitive):
+    def __init__(self, drop_refs=False, **kwargs):
         super().__init__(**kwargs)
 
         self.in_val = []
         self.in_crd = []
+        self.in_ref = []
 
         self.curr_crd = ''
+        self.curr_ref = ''
+        self.curr_val = ''
+        self.out_crds = ''
+        self.out_refs = ''
+        self.out_vals = ''
+        self.val_stkn_dropper = EmptyFiberStknDrop()
+        self.crd_stkn_dropper = EmptyFiberStknDrop()
+        self.ref_stkn_dropper = EmptyFiberStknDrop()
+        self.drop_refs = drop_refs
+
         if self.backpressure_en:
             self.ready_backpressure = True
             self.data_valid = True
@@ -49,13 +61,86 @@ class Compression(Primitive):
             if len(self.in_val) > 0 or len(self.in_crd) > 0:
                 self.block_start = False
 
-            icrd = ""
+            icrd = ''
+            ival = ''
 
             if self.done:
                 self.curr_crd = ''
+                self.curr_val = ''
+                self.out_crds = ''
+                self.out_vals = ''
+                if self.drop_refs:
+                    self.curr_ref = ''
+                    self.out_refs = ''
                 return
+            elif (len(self.in_val) > 0 and len(self.in_crd) == 0) or (len(self.in_crd) > 0 and len(self.in_val) == 0) or (len(self.in_val) == 0 and len(self.in_crd) == 0):
+                self.out_crds = ''
+                self.out_vals = ''
+                if self.drop_refs:
+                    self.out_refs = ''
             elif len(self.in_val) > 0 and len(self.in_crd) > 0:
-                icrd = self
+                ival = self.in_val.pop(0)
+                icrd = self.in_crd.pop(0)
+                iref = ''
+                if self.drop_refs:
+                    iref = self.in_ref.pop(0)
+
+                # print("ival:", ival)
+                # print("icrd:", icrd)
+
+                assert ival != '', "ival is an empty str"
+
+                if is_valid_num(ival):
+                    # assert isinstance(icrd, int), "both val and crd need ot match"
+                    if not isinstance(icrd, int) and icrd != 'N':
+                        print("Both val and icrd need to match")
+                        print(icrd, ival)
+                        exit(1)
+                    if ival == 0.0:
+                        self.curr_crd = ''
+                        self.curr_ref = ''
+                        self.curr_val = ''
+                    else:
+                        self.curr_crd = icrd
+                        self.curr_val = ival
+                        if self.drop_refs:
+                            self.curr_ref = iref
+                elif isinstance(ival, str) and ival != 'D':
+                    assert isinstance(icrd, str), "both val and coord need to match"
+                    self.curr_crd = icrd
+                    self.curr_val = ival
+                    if self.drop_refs:
+                        self.curr_ref = iref
+                elif ival == 'D':
+                    assert icrd == 'D'
+                    self.curr_val = ival
+                    self.curr_crd = icrd
+                    if self.drop_refs:
+                        self.curr_ref = iref
+                    self.done = True
+                else:
+                    self.curr_crd = icrd
+                    self.curr_val = ival
+                    if self.drop_refs:
+                        self.curr_ref = iref
+
+                # if self.curr_crd == self.out_crds or icrd == self.out_crds:
+                #     self.out_crds = ''
+                #     self.out_vals = ''
+                #     self.out_refs = ''
+                # else:
+                self.val_stkn_dropper.set_in_stream(self.curr_val)
+                self.crd_stkn_dropper.set_in_stream(self.curr_crd)
+                self.val_stkn_dropper.update()
+                self.crd_stkn_dropper.update()
+                # self.out_crds = self.crd_stkn_dropper.out_val()
+                # self.out_vals = self.val_stkn_dropper.out_val()
+                self.out_crds = self.curr_crd
+                self.out_vals = self.curr_val
+                if self.drop_refs:
+                    self.ref_stkn_dropper.set_in_stream(self.curr_ref)
+                    self.ref_stkn_dropper.update()
+                    self.out_refs = self.curr_ref
 
             if self.debug:
                 print("Curr OuterCrd:", self.curr_ocrd, "\tCurr InnerCrd:", icrd, "\t Curr OutputCrd:", self.curr_crd,
@@ -74,6 +159,20 @@ class Compression(Primitive):
         if self.backpressure_en:
             parent.set_backpressure(self.fifo_avail_crd)
 
+    def set_ref(self, ref, parent=None):
+        if ref != '' and ref is not None:
+            self.in_ref.append(ref)
+        if self.backpressure_en:
+            parent.set_backpressure(self.fifo_avail_crd)
+
     def out_crd(self):
         if (self.backpressure_en and self.data_valid) or not self.backpressure_en:
-            return self.curr_crd
+            return self.out_crds
+            
+    def out_ref(self):
+        if (self.backpressure_en and self.data_valid) or not self.backpressure_en:
+            return self.out_refs
+
+    def out_val(self):
+        if (self.backpressure_en and self.data_valid) or not self.backpressure_en:
+            return self.out_vals
